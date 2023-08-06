af5c5ab24b75e3778f5e61b3c38a314808e63006
Rittik Prasad <prasadrittik073@gmail.com>
2023-08-06 15:00:47 +0530

    update

--- index.js
+++ index.js
@@ -19,15 +19,16 @@ allowEIO3: true
 const port = 8080;
 
 const emailToSocketIdMap = new Map();
+
 const socketIdToEmailMap = new Map();
 
 /// Serve static files from the "dist" directory
 // app.use(express.static(path.join(__dirname, "../webView-Video-call/dist")));
 
 // Serve index.html for the root route
-app.get("/", (req, res, next) => {
-  res.send("<h4>kedhbjhwegdhwvedhw</h4>");
-});
+// app.get("/", (req, res, next) => {
+//   res.send("<h4>kedhbjhwegdhwvedhw</h4>");
+// });
 
 io.on("connection", (socket) => {
   console.log(`Socket Connected`, socket.id);
